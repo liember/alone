8ceedd6c98cc0516c395a9d2a79854f0daebd0e5
liember <liember@yandex.ru>
2020-07-11 03:23:48 +0700

    fix gender null able

--- regpage/models.py
+++ regpage/models.py
@@ -20,6 +20,7 @@ class User(AbstractUser):
                               error_messages = {
                                   'unique': "A user with that email already exists.",
                               })
+    gender = models.CharField(max_length=20, default = 'non filled')
     status = models.BooleanField(default=False)
     about = models.TextField(blank=True)
 
