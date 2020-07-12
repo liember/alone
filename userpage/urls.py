13295e606416aa38f575ff5161102b809bedf83e
liember <liember@yandex.ru>
2020-07-12 15:55:46 +0700

    stash before merge of "master" and "origin/master"

--- userpage/urls.py
+++ userpage/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
 #    path('admin/', admin.site.urls, name='admin_page'),
     path('', hello),
     path('<slug:username>', UserView.as_view(), name="user-page"),
+    path('edit', ProfileEditView.as_view(), name="edit"),
 ]
